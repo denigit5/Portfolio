c651ad9063d06370b5876b06eb7ed52c5206c4b0
Denyse Uwumukiza <denysemukiza@gmail.com>
2023-07-31 12:55:26 +0300

    Add mobile nagivation links

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.hamburger').addEventListener('click', () => {
+      document.querySelector('.navbar-links').classList.add('navbar-links-active');
+      document.querySelector('.navbar-links').classList.remove('navbar-links-inactive');
+    });
+    document.querySelector('.close-button').addEventListener('click', () => {
+      document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+      document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
+    });
+    document.querySelector('.nav-link1').addEventListener('click', () => {
+      document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+      document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
+    });
+    document.querySelector('.nav-link2').addEventListener('click', () => {
+      document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+      document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
+    });
+    document.querySelector('.nav-link3').addEventListener('click', () => {
+      document.querySelector('.navbar-links').classList.remove('navbar-links-active');
+      document.querySelector('.navbar-links').classList.add('navbar-links-inactive');
+    });
+});
