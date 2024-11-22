390663e61c51c25e436ae111ccce94a853d7064f
denigit5 <denysemukiza@gmail.com>
2024-11-22 07:38:36 +0300

    Add changes

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
       popupimage2: 'Assets/files/Snapshoot2.png',
       popupimage3: 'Assets/files/Snapshoot3.png',
       popupimage4: 'Assets/files/Snapshoot4.png',
-      popupimage: 'Assets/files/Snapshoot popuo desktop 1.png',
+      popupimage: 'Assets/Screenshot(82).png',
       mobiledescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
       popupmobiledescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
       popupdeskdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.',
